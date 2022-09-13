095d41c52aee4be3924b017e2c0957d8cd258926
Nthabi9 <nthabisengkoki9@gmail.com>
2022-09-12 17:10:01 -0700

    8.Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h.
 /**
  * main - print if the number is positive, zero, or negative
  *
@@ -13,7 +13,7 @@ for (c = '0'; c <= '9'; c++)
 {
 	putchar(c);
 }
-for (c = 'a'; c<= 'f'; c++)
+for (c = 'a'; c <= 'f'; c++)
 {
 	putchar(c);
 }
